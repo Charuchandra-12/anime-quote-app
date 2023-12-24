13ee538d4af1089602f885e0b096e701794ec217
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2023-12-24 21:24:22 +0530

    terraform-updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,77 +1,100 @@
-# resource "aws_vpc" "custom_vpc" {
-#   cidr_block = "10.0.0.0/16" 
-#   tags = {
-#     Name = "Prod-VPC"
-#   }
-# }
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
 
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.custom_vpc.id
-# }
+  tags = {
+    Name = "Custom VPC"
+  }
+}
 
-# resource "aws_route_table" "route_table" {
-#   vpc_id = aws_vpc.custom_vpc.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "1a"
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
-#   tags = {
-#     Name = "prod-RT"
-#   }
-# }
-# resource "aws_subnet" "custom_subnet" {
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+# resource "aws_subnet" "some_private_subnet" {
 #   vpc_id            = aws_vpc.custom_vpc.id
-#   cidr_block        = "10.0.1.0/24"  
-#   availability_zone = "us-east-1a"
+#   cidr_block        = "10.0.2.0/24"
+#   availability_zone = "1a"
+
 #   tags = {
-#     Name = "prod-subnet"
+#     Name = "Some Private Subnet"
 #   }
 # }
 
-# resource "aws_route_table_association" "route_table_asso" {
-#   subnet_id      = aws_subnet.custom_subnet.id
-#   route_table_id = aws_route_table.route_table.id
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+# resource "aws_route_table_association" "public_1_rt_a" {
+#   subnet_id      = aws_subnet.public_subnet.id
+#   route_table_id = aws_route_table.public_rt.id
 # }
 
-# resource "aws_security_group" "instance_sg" {
-#   name        = "webserver_sg"
-#   description = "Allow inbound SSH and HTTP traffic"
-#   # vpc_id      = aws_vpc.custom_vpc.id
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port   = 3000
-#     to_port     = 3000
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "Web-traffic"
-#   }
-# }
+resource "aws_security_group" "security_group" {
+  name   = "webserver_sg"
+  vpc_id = aws_vpc.custom_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_generated_key"
@@ -79,21 +102,21 @@ resource "aws_key_pair" "ssh_key" {
   
 }
 
-resource "aws_instance" "webserver" {
+resource "aws_instance" "web_instance" {
   ami               = "ami-0c7217cdde317cfec"
   instance_type     = "t2.xlarge"               
-  key_name          = aws_key_pair.ssh_key.key_name               
-  availability_zone = "us-east-1a"
-  # vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  key_name          = aws_key_pair.ssh_key.key_name
+
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.security_group.id]
+  associate_public_ip_address = true
+  user_data = file("script.sh")
+
   tags = {
     Name = "Web-Server"
   }
-  user_data = file("script.sh")
 }
+
 output "public_ip" {
-  value = aws_instance.webserver.public_ip
+  value = aws_instance.web_instance.public_ip
 }
-
-
-# https://www.sammeechward.com/terraform-vpc-subnets-ec2-and-more
-# actual code use the above link for the terraform code.
